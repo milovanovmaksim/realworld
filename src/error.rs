aacde9816c9f7690a081cb523ee73f959a745595
milovanovmaksim <milovanov160386@gmail.com>
2024-08-31 13:01:27 +0300

    implement AppError

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,73 @@
-use serde_json::Value as JsonValue;
+use actix_web::HttpResponse;
+use diesel::r2d2::{Error as R2D2Error, PoolError};
+use diesel::result::{DatabaseErrorKind, Error as DieselError};
+use serde_json::{json, Value as JsonValue};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum AppError {
+    // 401
+    #[error("Unauthorized: {}", _0)]
+    Unauthorized(JsonValue),
+
+    // 403
+    #[error("Forbidden: {}", _0)]
+    Forbidden(JsonValue),
+
+    // 404
+    #[error("Not Found: {}", _0)]
+    NotFound(JsonValue),
+
     // 422
     #[error("Unprocessable Entity: {}", _0)]
     UnprocessableEntity(JsonValue),
+
+    // 500
+    #[error("Internal Server Error")]
+    InternalServerError,
+}
+
+impl actix_web::error::ResponseError for AppError {
+    fn error_response(&self) -> actix_web::HttpResponse {
+        match self {
+            AppError::Unauthorized(msg) => HttpResponse::Unauthorized().json(msg),
+            AppError::Forbidden(msg) => HttpResponse::Forbidden().json(msg),
+            AppError::NotFound(msg) => HttpResponse::NotFound().json(msg),
+            AppError::UnprocessableEntity(msg) => HttpResponse::UnprocessableEntity().json(msg),
+            AppError::InternalServerError => {
+                HttpResponse::InternalServerError().json("Internal Server Error")
+            }
+        }
+    }
+}
+
+impl From<PoolError> for AppError {
+    fn from(_err: PoolError) -> Self {
+        AppError::InternalServerError
+    }
+}
+
+impl From<R2D2Error> for AppError {
+    fn from(_err: R2D2Error) -> Self {
+        AppError::InternalServerError
+    }
+}
+
+impl From<DieselError> for AppError {
+    fn from(err: DieselError) -> Self {
+        match err {
+            DieselError::DatabaseError(kind, info) => {
+                if let DatabaseErrorKind::UniqueViolation = kind {
+                    let message = info.details().unwrap_or_else(|| info.message()).to_string();
+                    AppError::UnprocessableEntity(json!({"error": message}))
+                } else {
+                    AppError::InternalServerError
+                }
+            }
+            DieselError::NotFound => {
+                AppError::NotFound(json!({"error": "requested record was not found"}))
+            }
+            _ => AppError::InternalServerError,
+        }
+    }
 }
