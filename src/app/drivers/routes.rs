c2319f61b5c4f6f5998e215dd4607e854b9d6022
milovanovmaksim <milovanov160386@gmail.com>
2024-08-31 13:41:04 +0300

    add route /tags

--- src/app/drivers/routes.rs
+++ src/app/drivers/routes.rs
@@ -4,9 +4,13 @@ use crate::app;
 
 pub fn api(cfg: &mut ServiceConfig) {
     cfg.service(
-        web::scope("/api").service(
-            web::scope("/healthcheck")
-                .route("", get().to(app::features::healthcheck::controllers::index)),
-        ),
+        web::scope("/api")
+            .service(
+                web::scope("/healthcheck")
+                    .route("", get().to(app::features::healthcheck::controllers::index)),
+            )
+            .service(
+                web::scope("/tags").route("", get().to(app::features::tag::controllers::index)),
+            ),
     );
 }
