7b5d83b72ebde7a9517ed0365617801643353530
milovanovmaksim <milovanov160386@gmail.com>
2024-09-08 18:54:47 +0300

    add type aliases

--- src/app/features/article/entities.rs
+++ src/app/features/article/entities.rs
@@ -2,11 +2,16 @@ use chrono::NaiveDateTime;
 use diesel::{
     associations::{Associations, Identifiable},
     deserialize::Queryable,
+    dsl::Eq,
 };
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::{app::features::user::entities::User, schema::articles};
+
+type WithAuthorId<T> = Eq<articles::author_id, T>;
+type WithSlug<T> = Eq<articles::slug, T>;
+type WithId<T> = Eq<articles::id, T>;
 
 #[derive(Identifiable, Queryable, Debug, Serialize, Deserialize, Associations, Clone)]
 #[diesel(belongs_to(User, foreign_key = author_id))]
