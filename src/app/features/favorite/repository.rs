391202117a58798515145cd1248b2049ea389015
milovanovmaksim <milovanov160386@gmail.com>
2024-09-21 15:00:49 +0300

    added FavoriteRepositoryImpl::unfavorite_article method

--- src/app/features/favorite/repository.rs
+++ src/app/features/favorite/repository.rs
@@ -7,11 +7,16 @@ use crate::{
     utils::db::DbPool,
 };
 
-use super::entities::{CreateFavorite, Favorite};
+use super::entities::{CreateFavorite, DeleteFavorite, Favorite};
 
 pub trait FavoriteRepository: Send + Sync + 'static {
     fn favorite_article(&self, user: User, article_title_slug: String)
         -> Result<Article, AppError>;
+    fn unfavorite_article(
+        &self,
+        user: User,
+        article_title_slug: String,
+    ) -> Result<Article, AppError>;
 }
 
 #[derive(Clone)]
@@ -42,6 +47,29 @@ impl FavoriteRepository for FavoriteRepositoryImpl {
         Favorite::create(
             conn,
             &CreateFavorite {
+                user_id: user.id,
+                article_id: article.id,
+            },
+        )?;
+        Ok(article)
+    }
+
+    fn unfavorite_article(
+        &self,
+        user: User,
+        article_title_slug: String,
+    ) -> Result<Article, AppError> {
+        let conn = &mut self.pool.get()?;
+        let article = Article::fetch_by_slug_and_author_id(
+            conn,
+            &FetchBySlugAndAuthorId {
+                slug: article_title_slug,
+                author_id: user.id,
+            },
+        )?;
+        Favorite::delete(
+            conn,
+            &DeleteFavorite {
                 user_id: user.id,
                 article_id: article.id,
             },
