41f4a7cf07aeab569f3873981bbc6a7398c92489
milovanovmaksim <milovanov160386@gmail.com>
2024-09-22 15:16:16 +0300

    added create controller for comment

--- src/app/features/comment/controllers.rs
+++ src/app/features/comment/controllers.rs
@@ -1,6 +1,9 @@
 use actix_web::{web, HttpRequest};
 
-use crate::{app::drivers::middlewares::state::AppState, utils::api::ApiResponse};
+use crate::{
+    app::drivers::middlewares::{auth, state::AppState},
+    utils::api::ApiResponse,
+};
 
 use super::request;
 
@@ -12,4 +15,11 @@ pub async fn create(
     path: web::Path<ArticleIdSlug>,
     form: web::Json<request::CreateCommentRequest>,
 ) -> ApiResponse {
+    let current_user = auth::get_current_user(&req)?;
+    let article_title_slug = path.into_inner();
+    let body = form.comment.body.to_owned();
+    state
+        .di_container
+        .comment_usecase
+        .create_comment(body, article_title_slug, current_user)
 }
