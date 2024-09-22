f6b88a4d7b7735274366424e63d14123bf233687
milovanovmaksim <milovanov160386@gmail.com>
2024-09-22 15:09:51 +0300

    added CommentRespositoryImpl::create_coment method

--- src/app/features/comment/repositories.rs
+++ src/app/features/comment/repositories.rs
@@ -1,10 +1,14 @@
 use crate::{
-    app::features::{profile::entities::Profile, user::entities::User},
+    app::features::{
+        article::entities::{Article, FetchBySlugAndAuthorId},
+        profile::entities::Profile,
+        user::entities::User,
+    },
     error::AppError,
     utils::db::DbPool,
 };
 
-use super::entities::Comment;
+use super::entities::{Comment, CreateComment};
 
 pub trait CommentRepository: Send + Sync + 'static {
     fn create_comment(
@@ -18,4 +22,32 @@ pub trait CommentRepository: Send + Sync + 'static {
 #[derive(Clone)]
 pub struct CommentRepositoryImpl {
     pool: DbPool,
+}
+
+impl CommentRepository for CommentRepositoryImpl {
+    fn create_comment(
+        &self,
+        body: String,
+        article_title_slug: String,
+        author: User,
+    ) -> Result<(Comment, Profile), AppError> {
+        let conn = &mut self.pool.get()?;
+        let article = Article::fetch_by_slug_and_author_id(
+            conn,
+            &FetchBySlugAndAuthorId {
+                slug: article_title_slug,
+                author_id: author.id,
+            },
+        )?;
+        let comment = Comment::create(
+            conn,
+            &CreateComment {
+                body,
+                author_id: author.id,
+                article_id: article.id,
+            },
+        )?;
+        let profile = author.fetch_profile(conn, &author.id)?;
+        Ok((comment, profile))
+    }
 }
